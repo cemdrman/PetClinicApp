12eb6510dda70d9f4435ed1be81166eca975d1a2
cem dırman <cemdrman@gmail.com>
2018-07-09 00:04:23 +0300

    updated for jpa entity

--- src/main/java/com/javaegitimleri/petclinic/model/Pet.java
+++ src/main/java/com/javaegitimleri/petclinic/model/Pet.java
@@ -2,22 +2,45 @@ package com.javaegitimleri.petclinic.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_name")
 public class Pet {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="petClinicSeqGen")
+	@SequenceGenerator(name="petClinicSeqGen",  sequenceName="petclinic_sequence")
 	private Long id;
-	private String neame;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="birth_date")
 	private Date birthDate;
+	
+	@ManyToOne
+	@JoinColumn(name="owner_id")
 	private Owner owner;
+	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getNeame() {
-		return neame;
+	public String getName() {
+		return name;
 	}
-	public void setNeame(String neame) {
-		this.neame = neame;
+	public void setNeame(String name) {
+		this.name = name;
 	}
 	public Date getBirthDate() {
 		return birthDate;
@@ -33,7 +56,7 @@ public class Pet {
 	}
 	@Override
 	public String toString() {
-		return "Pet [id=" + id + ", neame=" + neame + ", birthDate=" + birthDate + ", owner=" + owner + "]";
+		return "Pet [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", owner=" + owner + "]";
 	}
 	
 
